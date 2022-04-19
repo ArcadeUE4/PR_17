6f0f56b9cbf523e5692b7aa75c21733867b6708c
civilmob1234 <raiden123926@gmail.com>
2022-04-19 22:00:48 +0300

    Добавьте файлы проекта.

---
+++ PR_17/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Xamarin.Forms;
+
+namespace PR_17
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonPressed(object sender, EventArgs e)
+        {
+            if (entry.Text != "")
+            {
+                string url = "http://numbersapi.com";
+
+                using (var webClient = new WebClient())
+                {
+                    var reponse = webClient.DownloadString(url + "/" + entry.Text);
+                    label.Text = reponse;
+                }
+
+            }
+
+        }
+
+       
+    }
+}
